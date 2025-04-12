c2c43cfb7096725b4e4ba2051e0854c7063bb567
qingchengzai <3157470865@qq.com>
2025-04-12 19:26:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Intro
+This is a simple plugin which is real shit for trailing whitespace.
+The current features:
+1. highlight the trailing whitespaces
+2. delete the trailing whitespaces
+3. not yet
+
+# Install
+Like other nvim plugins.
+
+# Usage
+After installment, it needs to use require("shit-whitespace").setup({}) to config it.
+
+# Config
+Default config:
+```lua
+{
+	-- if true, enable trailing whitespaces highlight when WinNew event up
+	enable_highlight = false,
+	-- if true, enable automatic deletion when BufWritePre event up
+	enable_auto_delete = true,
+	-- if true, enable the default keymap of the plugin
+	enable_keymap = true,
+	-- set the trailing whitespace color
+	-- I like yellow, so I set it as the default
+	-- You could set it as the nvim way:
+	-- 1. color name: red yellow green ...
+	-- 2. number format1: #RRGGBB
+	-- 3. number format2: rgb{ number1, number2, number3 }
+	-- Note: put the color in '' or ""
+	-- Please look :h cterm-colors, :h gui-colors
+	ws_color = "yellow",
+}
+```
+# Command
+Default command:
+`ShitWSDelete`: Delete trailing whitespaces and allow the line address, like %ShitWSDelete, 1,2ShitWSDelete ...
+`ShitWSHighlight`: Enable highlight trailing whitespaces in current window
+`ShitWSNoHighlight`: Disable highlight trailing whitespaces in current window
+`ShitWSToggleHighlight`: Toggle highlight trailing whitespaces in current window
+
+# Map
+| lhs               | rhs              | meaning                                  | mode   |
+| :---:             | :---:            | :---:                                    | :---:  |
+| <leader>d\<Space> | ShitWSDelete<CR> | delete current line trailing whitespaces | normal |
+| <leader>h\<Space> | ShitWSToggle<CR> | toggle highlight trailing whitespaces    | normal |
+
+# Note
+This is simple and little plugin.
---
+++ lua/shit-whitespace.lua
@@ -0,0 +1,118 @@
+local M = {
+	config = {
+		enable_highlight = false,
+		enable_auto_delete = true,
+		enable_keymap = true,
+		ws_color = "yellow",
+	},
+}
+-- In setup, it will rewrite it by M.config.ws_color
+vim.api.nvim_set_hl(0, "ShitHITrailingWhiteSpace", { ctermbg = "yellow", bg = "yellow", force = true })
+
+local group = vim.api.nvim_create_augroup("ShitTrailingWhiteSpace", { clear = true })
+
+function M.ShitDeleteTrailingWhiteSpace(params)
+	local prestr = ""
+	if params.range == 1 then
+		prestr = string.format("%d", params.line1)
+	elseif params.range == 2 then
+		prestr = string.format("%d", params.line1) .. "," .. string.format("%d", params.line2)
+	end
+	vim.cmd(prestr .. "s/\\s\\+$//e")
+end
+
+function M.EnableHighlightWin()
+	vim.w.ShitWS_state = true
+	vim.cmd("match ShitHITrailingWhiteSpace /\\s\\+$/")
+end
+
+function M.DisableHighlightWin()
+	vim.w.ShitWS_state = false
+	vim.cmd("match ShitHITrailingWhiteSpace //")
+end
+
+function M.ToggleHighlightWin()
+	local res = pcall(vim.api.nvim_win_get_var, 0, "ShitWS_state")
+	if res then
+		if vim.w.ShitWS_state then
+			M.DisableHighlightWin()
+			vim.w.ShitWS_state = false
+		else
+			M.EnableHighlightWin()
+			vim.w.ShitWS_state = true
+		end
+	else
+		vim.w.ShitWS_state = true
+		M.EnableHighlightWin()
+	end
+end
+
+vim.api.nvim_create_user_command(
+	"ShitWSDelete",
+	M.ShitDeleteTrailingWhiteSpace,
+	{ desc = "shit-whitespace delete trailing whitespace", range = 2 }
+)
+vim.api.nvim_create_user_command(
+	"ShitWSHighlight",
+	M.EnableHighlightWin,
+	{ desc = "shit-whitespace start highlight trailing whitespace in current window" }
+)
+vim.api.nvim_create_user_command(
+	"ShitWSNoHighlight",
+	M.DisableHighlightWin,
+	{ desc = "shit-whitespace close highlight trailing whitespace in current window" }
+)
+vim.api.nvim_create_user_command(
+	"ShitWSToggleHighlight",
+	M.ToggleHighlightWin,
+	{ desc = "shit-whitespace toggle highlight trailing whitespace in current window" }
+)
+
+function M.setup(config)
+	vim.tbl_extend("force", M.config, config)
+	for k, v in pairs(config) do
+		M.config[k] = v
+	end
+	if M.config.enable_highlight then
+		vim.api.nvim_create_autocmd("WinNew", {
+			desc = "shit highlight the trailing whtespace",
+			group = group,
+			pattern = "*",
+			callback = function()
+				vim.cmd("match ShitHITrailingWhiteSpace /\\s\\+$/")
+				vim.w.ShitWS_state = true
+			end
+		})
+		M.EnableHighlightWin()
+	end
+	if M.config.enable_auto_delete then
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			desc = "shit delete the trailing whitespace",
+			group = group,
+			pattern = "*",
+			command = "%s/\\s\\+$//e",
+		})
+	end
+
+	vim.api.nvim_set_hl(
+		0,
+		"ShitHITrailingWhiteSpace",
+		{ ctermbg = M.config.ws_color, bg = M.config.ws_color, force = true }
+	)
+	if M.config.enable_keymap then
+		vim.keymap.set(
+			{ "n" },
+			"<leader>d ",
+			"<Cmd>ShitWSDelete<cr>",
+			{ desc = "shit-whitespace delete trailing whitespace", noremap = true, silent = true }
+		)
+		vim.keymap.set(
+			{ "n" },
+			"<leader>h ",
+			"<Cmd>ShitWSToggleHighlight<cr>",
+			{ desc = "shit-whitespace highlight trailing whitespace", noremap = true, silent = true }
+		)
+	end
+end
+
+return M
